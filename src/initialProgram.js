16a3d40b8818ea8418a8440641799b6208a744b5
pafalium <pedroalfaiate1992@hotmail.com>
2016-03-04 11:44:30 +0000

    Expanded initial program. Now it can be used to trigger more test cases.

--- src/initialProgram.js
+++ src/initialProgram.js
@@ -1,8 +1,16 @@
 
-module.exports = "\n\nvar c1 = cylinder( 2, 5 );\n\
+module.exports = "\n\n\
+function house(width, height, depth) {\n\
+	return group([\n\
+		box(width, height-2, depth), \n\
+		sphere(2/1)]);\n\
+}\n\
+var c1 = cylinder( 2, 5 );\n\
 var s1 = sphere( 5 );\n\
 move( s1, 0, 0, 10 );\n\
 var b1 = box( 4, 4, 4 );\n\
 var s2 = sphere( 2.5 );\n\
-var gr1 = group( b1, s2 );\n\
-move( gr1, 10, 0, 10 );";
+var gr1 = group([ b1, s2 ]);\n\
+move( gr1, 10, 0, 10 );\n\
+c1;\n\
+20;";
