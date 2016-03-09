9d8baa2d732f4bd28714b33f363f9c18931264ab
pafalium <pedroalfaiate1992@hotmail.com>
2016-03-09 17:41:39 +0000

    Optimized convertion to THREE.js for spheres, boxes and cylinders.

--- src/to-three.js
+++ src/to-three.js
@@ -11,57 +11,65 @@ var THREE = require('three');
 	THREE representation generation code.
 */
 function isRenderable(result) {
-	var renderables = {sphere: true, cylinder: true, box: true, move: true, group: true};
-	return renderables[result.name] === true;
+	return renderableFunctions[result.name] !== undefined;
 }
 
 function resultToThree(result) {
-	var renderableFunctions = {
-		sphere: function(result) {
-			var radius = result.args.radius;
-			var geom = new THREE.SphereGeometry(radius, 32, 32);
-			var mat = new THREE.MeshPhongMaterial();
-			var obj = new THREE.Mesh(geom, mat);
-			return obj;
-		},
-		cylinder: function(result) {
-			var radius = result.args.radius;
-			var height = result.args.height;
-			var geom = new THREE.CylinderGeometry(radius, radius, height, 32);
-			var mat = new THREE.MeshPhongMaterial();
-			var obj = new THREE.Mesh(geom, mat);
-			return obj;
-		},
-		box: function(result) {
-			var width = result.args.width,
-				height = result.args.height,
-				depth = result.args.depth;
-			var geom = new THREE.BoxGeometry(width, height, depth);
-			var mat = new THREE.MeshPhongMaterial();
-			var obj = new THREE.Mesh(geom, mat);
-			return obj;
-		},
-		move: function(result){
-			var objToMove = resultToThree(result.args.object);
-			var x = result.args.x,
-				y = result.args.z,//conversion
-				z = -result.args.y;//from XZ to XY ground plane
-			var obj = new THREE.Object3D();
-			obj.position.set(x,y,z);
-			obj.add(objToMove);
-			return obj;
-		},
-		group: function(result){
-			var objs = result.args.objects;
-			var threeObjs = objs.map(resultToThree);
-			var obj = new THREE.Object3D();
-			obj.add.apply(obj, threeObjs);
-			return obj;
-		}
-	};
 	return renderableFunctions[result.name](result);
 }
 
+
+var renderableFunctions = {
+	sphere: sphere,
+	cylinder: cylinder,
+	box: box,
+	move: move,
+	group: group
+};
+
+var solidMat = new THREE.MeshPhongMaterial();
+
+var sphereGeom = new THREE.SphereGeometry(1, 32, 32);
+function sphere(result) {
+	var radius = result.args.radius;
+	var obj = new THREE.Mesh(sphereGeom, solidMat);
+	obj.scale.set(radius, radius, radius);
+	return obj;
+}
+var cylinderGeom = new THREE.CylinderGeometry(1, 1, 1, 32);
+function cylinder(result) {
+	var radius = result.args.radius;
+	var height = result.args.height;
+	var obj = new THREE.Mesh(cylinderGeom, solidMat);
+	obj.scale.set(radius, height, radius);
+	return obj;
+}
+var boxGeom = new THREE.BoxGeometry(1, 1, 1);
+function box(result) {
+	var width = result.args.width,
+		height = result.args.height,
+		depth = result.args.depth;
+	var obj = new THREE.Mesh(boxGeom, solidMat);
+	obj.scale.set(width, height, depth);
+	return obj;
+}
+function move(result) {
+	var objToMove = resultToThree(result.args.object);
+	var x = result.args.x,
+		y = result.args.z, //conversion
+		z = -result.args.y; //from XZ to XY ground plane
+	var obj = new THREE.Object3D();
+	obj.position.set(x, y, z);
+	obj.add(objToMove);
+	return obj;
+}
+function group(result) {
+	var objs = result.args.objects;
+	var threeObjs = objs.map(resultToThree);
+	var obj = new THREE.Object3D();
+	obj.add.apply(obj, threeObjs);
+	return obj;
+}
 
 module.exports = {
 	isRenderable: isRenderable,
