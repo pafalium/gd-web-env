b841c484f8fe35cfe2cd53359bd26fddfa1cb9e1
pafalium <pedroalfaiate1992@hotmail.com>
2016-05-20 16:01:37 +0100

    Changed working 3d space for user programs. Now user programs always work in a Z is up space.

--- src/ReactComponents/ResultsView.jsx
+++ src/ReactComponents/ResultsView.jsx
@@ -9,6 +9,12 @@ class ResultsView extends React.Component {
 		let scene = new THREE.Scene();
 		// create three objects for the results
 		let threeObjects = toThree.convert.normally(this.props.results.results.topLevelExprResults.values());
+		// The program's results are in Z is up coordinates.
+		// They need to be rotated back into Y is up coordinates to fit OpenGL's standard.
+		// We do this by rotating all results -pi/2 radians around the x axis.
+		let objectGroup = new THREE.Object3D();
+		objectGroup.add(new THREE.Object3D(), ...threeObjects);
+		objectGroup.rotateOnAxis(new THREE.Vector3(1.0, 0.0, 0.0), -Math.PI/2.0);
 		// create lights
 		const sunLight = new THREE.DirectionalLight(0xffffca, 0.5);
 		sunLight.position.set(0.1, 1, 0.3);
@@ -21,7 +27,7 @@ class ResultsView extends React.Component {
 		const axisHelper = new THREE.AxisHelper(5);
 		axisHelper.quaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI/2.0)
 		// add everything
-		scene.add(sunLight, belowLight, hemiLight, gridHelper, axisHelper, ...threeObjects);
+		scene.add(sunLight, belowLight, hemiLight, gridHelper, axisHelper, objectGroup);
 		return scene;
 	}
 	render() {
--- src/SceneGraph/to-three.js
+++ src/SceneGraph/to-three.js
@@ -55,8 +55,14 @@ var renderableFunctions = {
 };
 
 var solidMat = new THREE.MeshPhongMaterial();
+// All geometries need to be rotated so that Z is their up axis.
+// They are defined with Y as their up axis so they need to be rotated -pi/2 around X.
+// This way Z becomes their up axis.
+var yUpToZUpRotation = (new THREE.Matrix4())
+	.makeRotationAxis(new THREE.Vector3(1.0, 0.0, 0.0), -Math.PI/2.0);
 
 var sphereGeom = new THREE.SphereGeometry(1, 32, 32);
+sphereGeom.applyMatrix(yUpToZUpRotation);
 function sphere(result, callback) {
 	var radius = result.args.radius;
 	var obj = new THREE.Mesh(sphereGeom, solidMat);
@@ -64,27 +70,29 @@ function sphere(result, callback) {
 	return obj;
 }
 var cylinderGeom = new THREE.CylinderGeometry(1, 1, 1, 32);
+cylinderGeom.applyMatrix(yUpToZUpRotation);
 function cylinder(result, callback) {
 	var radius = result.args.radius;
 	var height = result.args.height;
 	var obj = new THREE.Mesh(cylinderGeom, solidMat);
-	obj.scale.set(radius, height, radius);
+	obj.scale.set(radius, radius, height);
 	return obj;
 }
 var boxGeom = new THREE.BoxGeometry(1, 1, 1);
+boxGeom.applyMatrix(yUpToZUpRotation);
 function box(result, callback) {
 	var width = result.args.width,
 		height = result.args.height,
 		depth = result.args.depth;
 	var obj = new THREE.Mesh(boxGeom, solidMat);
-	obj.scale.set(width, depth, height);
+	obj.scale.set(width, height, depth);
 	return obj;
 }
 function move(result, callback) {
 	var objToMove = resultToThree(result.args.object, callback);
 	var x = result.args.x,
-		y = result.args.z, //conversion
-		z = -result.args.y; //from XZ to XY ground plane
+		y = result.args.y,
+		z = result.args.z;
 	var obj = new THREE.Object3D();
 	obj.position.set(x, y, z);
 	obj.add(objToMove);
@@ -94,10 +102,10 @@ function rotate(result, callback) {
 	var objToMove = resultToThree(result.args.object, callback);
 	var axis = result.args.axis,
 		angle = result.args.angle;
-	var yUpAxis = new THREE.Vector3(axis.x, axis.z, -axis.y);
 	var obj = new THREE.Object3D();
 	obj.add(objToMove);
-	obj.quaternion.setFromAxisAngle(yUpAxis, angle);
+	obj.quaternion.setFromAxisAngle(axis, angle);
+	return obj;
 	return obj;
 }
 
