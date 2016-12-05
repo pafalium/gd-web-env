4586f07cb82a9e0c55a1f282cc5544e350979fe8
pafalium <pedroalfaiate1992@hotmail.com>
2016-12-05 09:46:01 +0000

    Cleanup main app react component.

--- src/ReactComponents/App.jsx
+++ src/ReactComponents/App.jsx
@@ -1,22 +1,16 @@
 
-import React from 'react';
+import React, {PropTypes} from 'react';
 import Select from 'react-select';
 
-import RealTimeRunEditor from './RealTimeRunEditor.jsx';
-import TraceabilityView from './TraceabilityView.jsx';
 import SuperEditor from './SuperEditor.jsx';
 import {examples} from '../example-programs/examples.js';
 import {runInCad, clearCad, selectCads} from '../Runner/run-in-cad.js';
 
 import time from '../utils/time.js';
 
-const traceabilityMode = "traceability";
-const realtimeRunMode = "realtimeRun";
-const superMode = "superEditor";
-const modes = [superMode, traceabilityMode, realtimeRunMode];
-const availableCads = [
-	{value: "autocad", label: "AutoCAD"},
-	{value: "sketchup", label: "SketchUp"}
+const defaultAvailableCads = [
+	{value: "AutoCad", label: "AutoCAD"},
+	{value: "SketchUp", label: "SketchUp"}
 ];
 
 
@@ -28,37 +22,40 @@ class App extends React.Component {
 			activeProgram: examples[0],
 			activeProgramCurrentVersion: examples[0].program,
 			modeIdx: 0,
-			selectedCads: ["autocad"]
+			selectedCads: ["AutoCad"]
 		};
 		this.onSelectedProgram = this.onSelectedProgram.bind(this);
 		this.onProgramChange = this.onProgramChange.bind(this);
+		this.onRunInCad = this.onRunInCad.bind(this);
+		this.onClearCad = this.onClearCad.bind(this);
+		this.onSelectedCadsChange = this.onSelectedCadsChange.bind(this);
 	}
+
 	onSelectedProgram(program) {
 		this.setState({
 			activeProgram: program,
 			activeProgramCurrentVersion: program.program
 		});
 	}
+
 	onProgramChange(newProgram) {
 		this.setState({
 			activeProgramCurrentVersion: newProgram
 		});
 	}
-	onCycleMode() {
-		this.setState({
-			modeIdx: (this.state.modeIdx+1)%modes.length
-		});
-	}
+
 	onRunInCad() {
 		// Call run in cad procedure
 		selectCads(this.state.selectedCads);
 		time("CAD export", ()=>runInCad(this.state.activeProgramCurrentVersion));
 	}
+
 	onClearCad() {
 		// Call clear cad procedure
 		selectCads(this.state.selectedCads);
 		clearCad();
 	}
+
 	onSelectedCadsChange(selectedCads) {
 		let newSelectedCads = selectedCads !== null
 			? selectedCads.map(({value})=>value)
@@ -67,83 +64,94 @@ class App extends React.Component {
 			selectedCads: newSelectedCads
 		});
 	}
+
 	render() {
-		const currMode = modes[this.state.modeIdx];
-		const noCadSelected = this.state.selectedCads === null;
 		return (
 			<div style={{display: "flex"}}>
 				<div style={{width: "100px", overflowX: "scroll"}}>
-					<button 
-						onClick={this.onRunInCad.bind(this)}
-						disabled={noCadSelected}>
-						Run in CAD
-					</button>
-					<button 
-						onClick={this.onClearCad.bind(this)}
-						disabled={noCadSelected}>
-						Clear CAD
-					</button>
-					<Select 
-						multi={true}
-						options={availableCads}
-						value={this.state.selectedCads}
-						onChange={this.onSelectedCadsChange.bind(this)}
-						placeholder="destination CADs"
-					/>
+					<RunInCadControls 
+						onRunInCad={this.onRunInCad}
+						onClearCad={this.onClearCad}
+						onSelectedCadsChange={this.onSelectedCadsChange}
+						selectedCads={this.state.selectedCads}
+						availableCads={defaultAvailableCads}/>
 					<ProgramSelector 
 						programs={this.state.programs} 
 						onSelectedProgram={this.onSelectedProgram}/>
 				</div>
 				<div style={{width: "calc(100% - 100px)"}}>
-					{
-						currMode === traceabilityMode
-						? <TraceabilityView
+					<SuperEditor
 								program={this.state.activeProgram.program}
 								onProgramChange={this.onProgramChange}/>
-						: currMode === realtimeRunMode 
-							?	<RealTimeRunEditor
-									program={this.state.activeProgram.program}
-									onProgramChange={this.onProgramChange}/>
-							: <SuperEditor
-									program={this.state.activeProgram.program}
-									onProgramChange={this.onProgramChange}/>
-					}
 				</div>
 			</div>
 		);
 	}
 }
 
-class ProgramSelector extends React.Component {
-	renderProgramSelector(program) {
-		return (
-			<li style={styles.selectors.item}
-				onClick={this.props.onSelectedProgram.bind(null, program)}>
-				{program.name}
-			</li>
-		);
-	}
-	render() {
-		return (
-			<div>
-				<h2 style={styles.selectors.header}>Programs</h2>
-				<ul>
-					{this.props.programs.map(this.renderProgramSelector, this)}
-				</ul>
-			</div>
-		);
-	}
-}
 
-const styles = {
-	selectors: {
-		header: {
-
-		},
-		item: {
-			
-		}
-	}
+const RunInCadControls = ({ 
+  onRunInCad, 
+  onClearCad, 
+  onSelectedCadsChange, 
+  selectedCads,
+  availableCads 
+}) => (
+  <div>
+    <button 
+      onClick={onRunInCad}
+      disabled={selectedCads === null}>
+      Run in CAD
+    </button>
+    <button 
+      onClick={onClearCad}
+      disabled={selectedCads === null}>
+      Clear CAD
+    </button>
+    <Select 
+      multi={true}
+      options={availableCads}
+      value={selectedCads}
+      onChange={onSelectedCadsChange}
+      placeholder="destination CADs"
+    />
+  </div>
+);
+RunInCadControls.propTypes = {
+	onRunInCad: PropTypes.func.isRequired, 
+  onClearCad: PropTypes.func.isRequired, 
+  onSelectedCadsChange: PropTypes.func.isRequired, 
+  selectedCads: PropTypes.array.isRequired,
+  availableCads: PropTypes.array.isRequired
 };
+
+
+const ProgramSelectorItem = ({program, onClick}) => (
+	<li onClick={onClick}>
+		{program.name}
+	</li>
+);
+ProgramSelectorItem.propTypes = {
+	program: PropTypes.any.isRequired,
+	onClick: PropTypes.func.isRequired
+};
+
+
+const ProgramSelector = ({programs, onSelectedProgram}) => (
+	<div className="prog-selector">
+		<h2>Programs</h2>
+		<ul>
+			{programs.map(program => 
+				<ProgramSelectorItem program={program} 
+					onClick={onSelectedProgram.bind(null, program)} />
+			)}
+		</ul>
+	</div>
+);
+ProgramSelector.propTypes = {
+	programs: PropTypes.array.isRequired,
+	onSelectedProgram: PropTypes.func.isRequired
+};
+
 
 export default App;
