046627aa3736186a84cc9751c3cec2f51d645d08
pafalium <pedroalfaiate1992@hotmail.com>
2016-12-12 17:02:56 +0000

    Show name of current program on top of the page

---
+++ src/actions-reducers/CurrentProgramEditor.jsx
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import {connect} from 'react-redux';
+
+import {HorizontalBar} from '../ReactComponents/generic.jsx';
+
+import SuperEditor from '../ReactComponents/SuperEditor.jsx';
+import {changeProgram} from './actions-reducers.js';
+
+
+const ProgramName = ({name}) => (
+  <HorizontalBar>
+    <h1>{name}</h1>
+  </HorizontalBar>
+);
+
+const ProgramNameContainer = connect(
+  state => ({name: state.activeProgram.name})
+)(ProgramName);
+
+
+
+const SuperEditorContainer = connect(
+  state => ({
+    program: state.activeProgram.program
+  }),
+  dispatch => ({
+    onProgramChange: (program) => dispatch(changeProgram(program))
+  })
+)(SuperEditor);
+
+
+
+export const CurrentProgramEditor = () => (
+  <div>
+    <div style={{}}>
+      <ProgramNameContainer/>
+    </div>
+    <div style={{width: '100%', height: 'calc(100% - 3em)'}}>
+      <SuperEditorContainer/>
+    </div>
+  </div>
+);
+
+export default CurrentProgramEditor;
--- src/actions-reducers/EditorPage.jsx
+++ src/actions-reducers/EditorPage.jsx
@@ -1,21 +1,9 @@
 
 import React from 'react';
-import {connect} from 'react-redux';
 
 import SidePanels from './components.jsx';
-import SuperEditor from '../ReactComponents/SuperEditor.jsx';
+import CurrentProgramEditor from './CurrentProgramEditor.jsx';
 
-import {changeProgram} from './actions-reducers.js';
-
-
-const SuperEditorContainer = connect(
-  state => ({
-    program: state.activeProgram.program
-  }),
-  dispatch => ({
-    onProgramChange: (program) => dispatch(changeProgram(program))
-  })
-)(SuperEditor);
 
 const EditorPage = () => (
   <div>
@@ -32,7 +20,7 @@ const EditorPage = () => (
       width: 'calc(100% - 80px)', 
       height: '100%',
       zIndex: '1'}}>
-      <SuperEditorContainer/>
+      <CurrentProgramEditor/>
     </div>
   </div>
 );
